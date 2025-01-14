b1acc5191effe93375e25b693d8a40a57a06bbc5
enrique <enrique@nevermined.io>
2025-01-14 16:32:58 +0100

    feat: update env var name

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,6 @@ export const NVM_API_KEY = process.env.NVM_API_KEY!;
 export const NVM_ENVIRONMENT = process.env.NVM_ENVIRONMENT || "testing";
 export const THIS_PLAN_DID = process.env.THIS_PLAN_DID!;
 export const VIDEO_GENERATOR_PLAN_DID = process.env.VIDEO_GENERATOR_PLAN_DID!;
-export const THIS_AGENT_DID = process.env.THIS_AGENT_DID!;
+export const AGENT_DID = process.env.AGENT_DID!;
 export const SCRIPT_GENERATOR_DID = process.env.SCRIPT_GENERATOR_DID!;
 export const VIDEO_GENERATOR_DID = process.env.VIDEO_GENERATOR_DID!;
